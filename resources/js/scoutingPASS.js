30845ec39861014c90758fdc561e657d08bc4763
Matthew LePrell <murpful@gmail.com>
2025-03-24 00:32:42 -0400

    structure changes

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -965,7 +965,6 @@ function clearForm() {
     if (code == "m") continue
     if (code.substring(0, 2) == "r_") continue
     if (code.substring(0, 2) == "l_") continue
-    if (code == "e") continue
     if (code == "s") continue
 
     if (e.className == "clickableImage") {
